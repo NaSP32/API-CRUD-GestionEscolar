3185c4b31a0ee5d60a6b6e8364105326af493c94
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-11-08 19:28:03 -0300

    agrego cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,8 @@
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173/",
-    "https://gestor-estudiantes-front.vercel.app/"
+    "http://localhost:5173",
+    "https://gestor-estudiantes-front.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type"],
