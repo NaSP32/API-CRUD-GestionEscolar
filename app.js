149f022a90f50c8e8405f6c6e84f16ea55caece9
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-11-08 19:25:49 -0300

    agrego cors

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors"); // 1. Importar el paquete cors
 const connectDB = require("./config/db");
 const estudiantesRoutes = require("./routes/estudiantes");
+const corsOptions = require("./config/corsOptions"); // 2. Importar las opciones de CORS
 
 dotenv.config(); //cargar variables de entorno
 connectDB(); //conectar a la bd
 
 
 const app = express();
+//Configuración de cors
+app.use(cors(corsOptions)); // 3. Usar el middleware de CORS con las opciones definidas
 app.use(express.json()); //Middleware para leer json
-
-const express = require('express');
-const cors = require('cors');
-
-
-
-// ✅ AGREGA ESTO ANTES DE TUS RUTAS
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://api-crud-gestion-escolar.vercel.app/' // Si después despliegas el front
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
 
 
 
---
+++ config/corsOptions.js
@@ -0,0 +1,12 @@
+//config/corsOptions.js
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173/",
+    "https://gestor-estudiantes-front.vercel.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type"],
+};
+
+module.exports = corsOptions;
