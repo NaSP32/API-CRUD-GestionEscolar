e05b67a9500d70db48d376db276bbaf6d1b56812
Nadia Soledad Pereyra <nadiapereyra32@gmail.com>
2025-11-08 17:51:48 -0300

    agrego cors

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ connectDB(); //conectar a la bd
 const app = express();
 app.use(express.json()); //Middleware para leer json
 
+const express = require('express');
+const cors = require('cors');
+
+
+
+// ✅ AGREGA ESTO ANTES DE TUS RUTAS
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://api-crud-gestion-escolar.vercel.app/' // Si después despliegas el front
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+
+
+
 //Ruta base
 app.get("/", (req, res) => {
     res.send('API de Gestión Escolar')
